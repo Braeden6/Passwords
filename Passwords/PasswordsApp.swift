b8ea56f39a99c399e6290b98a0662cb7a7de4132
Braeden <braeden.norman6@gmail.com>
2021-06-23 20:32:53 -0700

    Initial Commit

---
+++ Passwords/PasswordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordsApp.swift
+//  Passwords
+//
+//  Created by Braeden Norman on 2021-06-23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
