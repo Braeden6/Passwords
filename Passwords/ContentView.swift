2a9428b7899e1fae8695049ae872d7964e31fba3
Braeden <braeden.norman6@gmail.com>
2021-06-27 22:41:41 -0700

    Add delete function to Password list and start option popups for editing password field.

--- Passwords/ContentView.swift
+++ Passwords/ContentView.swift
@@ -9,117 +9,92 @@ import SwiftUI
 import Swifter
 
 
-struct PasswordInfo: Codable, Identifiable {
-    var id = UUID()
-    var identifier: String
-    var username: String
-    var password: String
-    var description: String
-    init() {
-        self.username = ""
-        self.password = ""
-        self.description = ""
-        self.identifier = ""
-    }
-}
-
-
-
-
-class PasswordList: ObservableObject {
-    @Published var list: [PasswordInfo] = getSavedPasswordList()
-    
-    func addCopyOfPasswordInfo(passwordInfo: PasswordInfo) {
-        var newPassInfo = PasswordInfo()
-        newPassInfo.username = passwordInfo.username
-        newPassInfo.identifier = passwordInfo.identifier
-        newPassInfo.description = passwordInfo.description
-        newPassInfo.password = passwordInfo.password
-        list.append(newPassInfo)
-    }
-}
 
 struct ContentView: View {
     @State var passwordInfo = PasswordInfo()
     @StateObject  var passwordList = PasswordList()
-    @State var server: HttpServer = serverStartup()
+    @State var server: HttpServer = serverInit()
+    @State var ipAddress = UIDevice.current.getIP()
+    @State var show = false
+    
     var body: some View {
-        List(passwordList.list) { passwordInfo in
-            Text("Your password is: \(passwordInfo.password)")
+        VStack() {
+            ScrollView {
+                VStack() {
+                    ForEach(passwordList.list, id: \.self) { passwordInfo in
+                        PasswordInfoRow(passwordInfo: passwordInfo, list: passwordList)
+                    }
+                }
+            }
+            VStack(){
+                let usernameTF: TextField = TextField("Username", text: $passwordInfo.username)
+                usernameTF.frame(width: 200, height: 20, alignment: .center)
+                let passwordTF: TextField = TextField("Password", text: $passwordInfo.password)
+                passwordTF.frame(width: 200, height: 20, alignment: .center)
+                let descriptionTF: TextField = TextField("Description", text: $passwordInfo.description)
+                descriptionTF.frame(width: 200, height: 20, alignment: .center)
+                let identifierTF: TextField = TextField("Identifier", text: $passwordInfo.identifier)
+                identifierTF.frame(width: 200, height: 20, alignment: .center)
+            }
+            
+            
+            Button(action: { buttonEnterCallback()})
+            { Text("Enter") }
+            
+            Button(action :{passwordList.resetList() })
+            { Text("Reset List") }
+            
+            Button(action :{ try! server.start() })
+            { Text("Server Start") }
+            
+            Button(action :{ server.stop() })
+            { Text("Stop Server") }
+            
+            Button(action :{ self.show.toggle() })
+            { Text("Toggle") }
+            
+            Text(ipAddress!)
+        }.opacity(self.show ? 0 : 1)
+        
+        if self.show {
+            GeometryReader{_ in
+                PasswordOptionsPopup()
+            }.background (
+                Color.white.opacity(0.65)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {})
+            
+            Button(action: { withAnimation {
+                self.show.toggle()
+            }})
+            { Text("Close") }
         }
-        let usernameTF: TextField = TextField("Username", text: $passwordInfo.username)
-        usernameTF.frame(width: 200, height: 20, alignment: .center)
-        let passwordTF: TextField = TextField("Password", text: $passwordInfo.password)
-        passwordTF.frame(width: 200, height: 20, alignment: .center)
-        let descriptionTF: TextField = TextField("Description", text: $passwordInfo.description)
-        descriptionTF.frame(width: 200, height: 20, alignment: .center)
-        let identifierTF: TextField = TextField("Identifier", text: $passwordInfo.identifier)
-        identifierTF.frame(width: 200, height: 20, alignment: .center)
-        
-        Button(action: {
-            buttonEnterCallback(passwordList: passwordList, passwordInfo: &passwordInfo)
-            savePasswordList(list: passwordList.list)
-        }) {
-            Text("Enter")
-        }
-        
-        Button(action :{
-            savePasswordList(list: [])
-            passwordList.list = []
-        }) {
-            Text("Reset List")
-        }
-        .frame(width: 200, height: 20, alignment: .center)
-        
-        
-        Button(action: {
-            print(UIDevice.current.getIP()) }) {
-            Text("Get IP")
-        }
-        
         
         
     }
-}
-
-func buttonEnterCallback(passwordList: PasswordList, passwordInfo: inout PasswordInfo) {
-    passwordList.addCopyOfPasswordInfo(passwordInfo: passwordInfo)
-    passwordInfo.password = ""
-    passwordInfo.username = ""
-    passwordInfo.description = ""
-    passwordInfo.identifier = ""
-}
-
-func savePasswordList(list: [PasswordInfo]) {
-    let encoder = JSONEncoder()
-    if let encoded = try? encoder.encode(list) {
-        let defaults = UserDefaults.standard
-        defaults.set(encoded, forKey: "passwordList")
+    
+    func buttonEnterCallback() {
+        passwordList.addCopyOfPasswordInfo(passwordInfo: passwordInfo)
+        passwordInfo.password = ""
+        passwordInfo.username = ""
+        passwordInfo.description = ""
+        passwordInfo.identifier = ""
     }
 }
 
-// On success gets a list of PasswordInfo otherwise returns []
-func getSavedPasswordList() -> [PasswordInfo] {
-    let defaults = UserDefaults.standard
-    if let list = defaults.object(forKey: "passwordList") as? Data {
-        let decoder = JSONDecoder()
-        if let passwordList = try? decoder.decode([PasswordInfo].self, from: list) {
-            return passwordList
-        }
-    }
-    return []
-}
 
-
-func serverStartup() -> HttpServer{
+func serverInit() -> HttpServer{
     let server = HttpServer()
     server[""] = scopes {
         html {
-          body {
+            body {
             h1 { inner = "Your password will show up here" }
           }
         }
       }
-    try! server.start()
     return server
 }
+
+
+
+
---
+++ Passwords/PasswordOptionsPopup.swift
@@ -0,0 +1,19 @@
+//
+//  PasswordOptionsPopup.swift
+//  Passwords
+//
+//  Created by Braeden Norman on 2021-06-27.
+//
+
+import Foundation
+import SwiftUI
+
+    
+struct PasswordOptionsPopup : View {
+    @State var show: Bool = false
+    
+    
+    var body: some View {
+        Text("Enter")
+    }
+}
